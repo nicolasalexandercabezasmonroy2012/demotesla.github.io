056345c6b2dfa32210ae9c376eecd4951cf1d4fb
nicolasalexandercabezasmonroy2012 <114183851+nicolasalexandercabezasmonroy2012@users.noreply.github.com>
2022-09-26 14:13:31 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+function changeimages(idimages){
+    var car = document.getElementById('car')
+    if(idimages==1){
+      car.src='images/white.png'
+    }
+    var car = document.getElementById('car')
+    if(idimages==2){
+      car.src='images/blue.png'
+    }
+    var car = document.getElementById('car')
+    if(idimages==3){
+      car.src='images/gray.png'
+    }
+    var car = document.getElementById('car')
+    if(idimages==4){
+      car.src='images/black.png'
+    }
+    var car = document.getElementById('car')
+    if(idimages==5){
+      car.src='images/red.png'
+    }
+  }
